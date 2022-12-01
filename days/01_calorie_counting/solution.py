5b5ddef3593842db7d581b05c8687e61aad3b9e2
Valerii Zhyla <orator9x@gmail.com>
2022-12-01 14:08:13 +0100

    Template for daily solutions

---
+++ days/01_calorie_counting/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    input = []
+
+    def __init__(self, input):
+        self.input = input
+
+    def solve(self):
+        pass
+
+
+def read_input() -> list[str]:
+    input_file_name = 'input.txt'
+    try:
+        with open(input_file_name) as file:
+            input_lines = file.readlines()
+            if len(input_lines) == 0:
+                raise Exception(f"File {input_file_name} is empty")
+            input_lines_without_newline = [line.rstrip('\n') for line in input_lines]
+            return input_lines_without_newline
+    except EnvironmentError:
+        raise Exception(f"File {input_file_name} is missing or invalid")
+
+
+if __name__ == '__main__':
+    input_lines = read_input()
+    print(input_lines)
+    solution = Solution(input_lines)
+    solution.solve()
+
