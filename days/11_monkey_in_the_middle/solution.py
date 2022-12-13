999dbdac0d6c69d62efdf381ce9f211104b5b6cc
Orator <orator9x@gmail.com>
2022-12-13 20:26:34 +0100

    Day 11: task 2 solved

--- days/11_monkey_in_the_middle/solution.py
+++ days/11_monkey_in_the_middle/solution.py
@@ -4,6 +4,7 @@ from model.item import Item
 from model.operation import Operation, Add, Square, MultiplyBy
 import copy
 
+
 class Solution:
     monkeys: list[Monkey] = []
 
@@ -12,21 +13,24 @@ class Solution:
 
     def solve(self) -> (int, int):
         # task 1
-        #monkey_interaction_short = MonkeyInteraction(copy.deepcopy(self.monkeys))
-        #monkey_interaction_short.perform_rounds(20)
+        monkey_interaction_short = MonkeyInteraction(copy.deepcopy(self.monkeys))
+        monkey_interaction_short.perform_rounds(20)
         # task 2
         monkey_interaction_long = MonkeyInteractionLong(copy.deepcopy(self.monkeys))
         monkey_interaction_long.perform_rounds(10000)
-        # return monkey_interaction_short.get_monkey_business(), monkey_interaction_long.get_monkey_business()
-        return 1, monkey_interaction_long.get_monkey_business()
+        return monkey_interaction_short.get_monkey_business(), monkey_interaction_long.get_monkey_business()
 
 
 def read_input() -> list[Monkey]:
     return [
-        Monkey(0, [Item(79), Item(98)], MultiplyBy(19), 23, 2, 3),
-        Monkey(1, [Item(54), Item(65), Item(75), Item(74)], Add(6), 19, 2, 0),
-        Monkey(2, [Item(79), Item(60), Item(97)], Square(), 13, 1, 3),
-        Monkey(3, [Item(74)], Add(3), 17, 0, 1),
+        Monkey(0, [Item(84), Item(72), Item(58), Item(51)], MultiplyBy(3), 13, 1, 7),
+        Monkey(1, [Item(88), Item(58), Item(58)], Add(8), 2, 7, 5),
+        Monkey(2, [Item(93), Item(82), Item(71), Item(77), Item(83), Item(53), Item(71), Item(89)], Square(), 7, 3, 4),
+        Monkey(3, [Item(81), Item(68), Item(65), Item(81), Item(73), Item(77), Item(96)], Add(2), 17, 4, 6),
+        Monkey(4, [Item(75), Item(80), Item(50), Item(73), Item(88)], Add(3), 5, 6, 0),
+        Monkey(5, [Item(59), Item(72), Item(99), Item(87), Item(91), Item(81)], MultiplyBy(17), 11, 2, 3),
+        Monkey(6, [Item(86), Item(69)], Add(6), 3, 1, 0),
+        Monkey(7, [Item(91)], Add(1), 19, 2, 5),
     ]
 
 
@@ -34,4 +38,4 @@ if __name__ == '__main__':
     input_lines = read_input()
     solution = Solution(input_lines)
     print(f"Answer : {solution.solve()}")
-    # 55458
+    # 55458, 14508081294
